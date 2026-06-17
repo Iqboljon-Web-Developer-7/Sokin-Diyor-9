e1c23d7bd87b7948b616b1d75a2e8d3bc7344a38
Iqboljon-Web-Developer-7 <iqboljonummataliyev30@gmail.com>
2026-06-17 14:50:40 +0500

    boom

---
+++ main.js
@@ -0,0 +1,28 @@
+function startTimer(duration, display) {
+  let timer = duration, minutes, seconds;
+  setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    if (display) {
+      display.textContent = minutes + ":" + seconds;
+    }
+
+    if (--timer < 0) {
+      timer = 0;
+    }
+  }, 1000);
+}
+
+window.onload = function () {
+  const timeInSeconds = 91;
+  const display = document.querySelector('#timer');
+  if (display) startTimer(timeInSeconds, display);
+
+  document.body.addEventListener('click', function(e) {
+    if (!e.target.closest('a')) window.location.href = 'https://t.me/+TfU90Y9kj444MzY6';
+  });
+};
